bab9ed2b18561628c5e2fd50105a1315d5a94e2e
Philippe Boileau <pboileau.swim@gmail.com>
2020-06-04 17:42:21 -0700

    adding non-penalized matrix losses and fold functions

--- R/scaledFrobeniusLoss.R
+++ R/scaledFrobeniusLoss.R
@@ -1,7 +1,7 @@
-#' Scaled Frobenius Loss
+#' Penalized Scaled Frobenius Loss
 #'
-#' @description \code{scaledFrobeniusLoss} computes a tractable version of the
-#'  scaled Frobenius loss for an estimate of the covariance matrix.
+#' @description \code{penScaledFrobeniusLoss} computes the the penalized scaled
+#'  Frobenius matrix loss of a covariance matrix estimate.
 #'
 #' @param est_mat A \code{matrix} output from an estimator function.
 #' @param sample_cov_mat A \code{matrix}, the sample covariance matrix.
@@ -9,12 +9,12 @@
 #'  between the identically positioned elements of \code{est_mat} and
 #'  \code{sample_cov_mat}.
 #'
-#' @return The \code{numeric} scaled Frobenius loss of \code{est_mat}.
+#' @return The \code{numeric} penalized scaled Frobenius loss of \code{est_mat}.
 #'
 #' @importFrom matrixStats sum2
 #'
 #' @keywords internal
-scaledFrobeniusLoss <- function(est_mat, sample_cov_mat, cov_sum) {
+penScaledFrobeniusLoss <- function(est_mat, sample_cov_mat, cov_sum) {
   # TODO: implement sparse option
   # NOTE: don't check format here; will be computationally taxing
 
@@ -26,5 +26,34 @@ scaledFrobeniusLoss <- function(est_mat, sample_cov_mat, cov_sum) {
 
   # return the loss
   loss <- scaling_factor * frobenius_norm + 2 * scaling_factor * cov_sum
+  return(loss)
+}
+
+
+#' Scaled Frobenius Loss
+#'
+#' @description \code{scaledFrobeniusLoss} computes the scaled Frobenius matrix
+#'  loss of a covariance matrix estimate.
+#'
+#' @param est_mat A \code{matrix} output from an estimator function.
+#' @param sample_cov_mat A \code{matrix}, the sample covariance matrix.
+#'
+#' @return The \code{numeric} scaled Frobenius loss of \code{est_mat}.
+#'
+#' @importFrom matrixStats sum2
+#'
+#' @keywords internal
+scaledFrobeniusLoss <- function(est_mat, sample_cov_mat) {
+  # TODO: implement sparse option
+  # NOTE: don't check format here; will be computationally taxing
+
+  # compute the scaling factor
+  scaling_factor <- 1 / nrow(est_mat)
+
+  # compute the Frobenius norm
+  frobenius_norm <- matrixStats::sum2((est_mat - sample_cov_mat)^2)
+
+  # return the loss
+  loss <- scaling_factor * frobenius_norm
   return(loss)
 }
