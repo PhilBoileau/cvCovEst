a7f4f675596df110103c7e7350c5491ed298afd8
Brian Collica <bcollica@berkeley.edu>
2020-11-10 13:44:28 -0800

    Adding function, bestInClass, to show the best perfomring estimator within each class.

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -1,6 +1,8 @@
 ################################################################################
 # Plotting Functions for cvCovEst
 
+
+################################################################################
 #' Summary Statistics of Empirical Risk by Estimator Class
 #'
 #' @description The \code{empRiskByClass} function calculates the following
@@ -31,5 +33,33 @@ empRiskByClass <- function(dat) {
     dplyr::arrange(mean_risk)
 
   return(empRisk)
+}
+
+################################################################################
+#' Showing Best Estimator Within Each Class of Estimators
+#'
+#' @description The \code{bestInClass} function finds the best performing
+#'  estimator within each class of estimator passed to \code{cvCovEst} and
+#'  finds the associated hyperparameters if applicable.
+#'
+#' @param dat The table of empirical risk calculations which is output by
+#' \code{cvCovEst}.
+#'
+#' @return A \code{data.frame} with rows corresponding to estimator classes and
+#'  columns for hyperparameter values and empirical risk for the best estimator
+#'  in that class.
+#'
+#' @importFrom dplyr group_by summarize arrange first %>%
+#'
+#' @keywords internal
+bestInClass <- function(dat) {
+  bestEst <- dat %>%
+    dplyr::group_by(estimator) %>%
+    dplyr::summarise(hyperparameter = first(hyperparameters),
+                     empirical_risk = first(empirical_risk),
+                     .groups = "keep") %>%
+    dplyr::arrange(empirical_risk)
+
+  return(bestEst)
 }
 
