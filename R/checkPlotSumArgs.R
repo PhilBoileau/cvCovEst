d2a625cd4ad97bb90ba5819ffb91805df2496095
Brian Collica <bcollica@berkeley.edu>
2021-01-27 09:07:04 -0800

    Split similar functions into separate files

---
+++ R/checkPlotSumArgs.R
@@ -0,0 +1,111 @@
+#' Check Arguments Passed to plot.cvCovEst and summary.cvCovEst
+#'
+#' @description The \code{checkPlotSumArgs} function verifies that all arguments
+#' passed to the \code{plot.cvCovEst} and \code{summary.cvCovEst} functions meet
+#' their specifications.  Some additional arguments may be checked at the
+#' individual function level.
+#'
+#' @param dat An object of class, \code{"cvCovEst"}.  Specifically, this is the
+#'  standard output of the function \code{cvCovEst}.
+#' @param dat_orig The numeric \code{data.frame}, \code{matrix}, or similar
+#'  object originally passed to \code{cvCovEst}.
+#' @param which_fun A choice of \code{"plot"} or \code{"summary"} depending on.
+#'  which function is being checked.
+#' @param estimator A character vector specifying one or more classes of
+#'  estimators to compare.
+#' @param plot_type A character vector specifying one of four choices of
+#'  diagnostic plots.
+#' @param summ_fun A character vector specifying which summaries to output.
+#' @param stat A character vector of one or more summary statistics to use when
+#'  comparing estimators.
+#' @param k The number of leading/trailing eigenvalues to plot.
+#' @param leading A \code{logical} indicating if the leading eigenvalues should
+#'  be used.
+#' @param abs_v A \code{logical} determining if the absolute value of the matrix
+#'  entries should be used for plotting the matrix heatmap.
+#'
+#' @import assertthat
+#'
+#' @return Whether all argument conditions are satisfied.
+#'
+#' @keywords internal
+checkPlotSumArgs <- function(
+  dat,
+  dat_orig,
+  which_fun,
+  estimator,
+  plot_type,
+  summ_fun,
+  stat,
+  k,
+  leading,
+  abs_v) {
+
+  # Define possible valid arguments for both functions
+  cv_names <- c("estimate", "estimator", "risk_df", "cv_df", "args")
+
+  # Check that names of dat match what is expected of cvCovEst output
+  if (is.cvCovEst(dat)) {
+    assertthat::assert_that(
+      all(cv_names %in% names(dat)) == TRUE,
+      msg = paste(
+        "cvCovEst object is missing data.",
+        "Make sure that the cvCovEst object possess the following elements:",
+        "'estimate', 'estimator', 'risk_df', 'cv_df', and 'args'."
+      )
+    ) # change output message here?
+  }
+  # For plot functions only:
+  if (which_fun == "plot") {
+    # Define valid plot arguments
+    plot_types <- c("summary", "risk", "eigen", "heatmap")
+    stat_choices <- c("min", "Q1", "median", "Q3", "max")
+    cv_estimators <- unique(dat$risk_df$estimator)
+
+    # Check valid summary stat choices
+    assertthat::assert_that(
+      all(stat %in% stat_choices) == TRUE,
+      msg = "Non-supported summary statistic provided.")
+    # Check valid estimators - estimators must have been passed through cvCovEst
+    assertthat::assert_that(
+      all(estimator %in% cv_estimators) == TRUE,
+      msg = "Can only use estimators passed to the cvCovEst function.")
+    # Check valid k values
+    if (!is.null(k)) {
+      assertthat::assert_that(
+        k <= ncol(dat_orig),
+        msg = "k cannot exceed the number of columns in dat_orig."
+      )
+    }
+    # Check valid plot types
+    assertthat::assert_that(
+      all(
+        plot_type %in% plot_types,
+        length(plot_type) == 1) == TRUE,
+      msg = "Must provide a single valid plot type.")
+    # Check valid logicals
+    assertthat::assert_that(
+      all(
+        is.logical(leading),
+        is.logical(abs_v)
+      ) == TRUE,
+      msg = "Invalid value for logical argument."
+    )
+    # Check valid summary plot conditions
+    if (plot_type == "summary" & !is.null(estimator)) {
+      message("Summary plot defaults to the optimal selected estimator.")
+    }
+  }
+  # For summary functions only (which_fun == 'summary')
+  else{
+    # Define valid summary functions
+    summary_functions <- c(
+      "empRiskByClass", "bestInClass", "worstInClass", "hyperRisk")
+
+    # Check valid summary functions
+    assertthat::assert_that(
+      all(summ_fun %in% summary_functions) == TRUE,
+      msg = "Must provide a valid summary function."
+    )
+  }
+}
---
+++ R/summary_functions.R
@@ -0,0 +1,218 @@
+# Main Summary Method and Summary Functions for cvCovEst
+
+################################################################################
+#' Summary Statistics of Empirical Risk by Estimator Class
+#'
+#' @description The \code{empRiskByClass} function calculates the following
+#'  summary statistics for the empirical risk within each class of estimator
+#'  passed to \code{cvCovEst}: minimum, Q1, median, mean, Q3, and maximum.  The
+#'  results are output as a \code{tibble}.
+#'
+#' @param dat The table of empirical risk calculations which is output by
+#'  \code{cvCovEst}.
+#'
+#' @return A \code{data.frame} with rows corresponding to estimator classes and
+#'  columns corresponding to each summary statistic.
+#'
+#' @importFrom dplyr group_by summarize arrange %>%
+#' @importFrom stats median quantile
+#' @importFrom rlang .data
+#'
+#' @keywords internal
+empRiskByClass <- function(dat) {
+  emp_risk <- dat %>%
+    dplyr::group_by(.data$estimator) %>%
+    dplyr::summarise(
+      min = min(.data$empirical_risk),
+      Q1 = quantile(.data$empirical_risk, probs = 0.25, type = 3),
+      median = quantile(.data$empirical_risk, probs = 0.5, type = 3),
+      Q3 = quantile(.data$empirical_risk, probs = 0.75, type = 3),
+      max = max(.data$empirical_risk),
+      mean_risk = mean(.data$empirical_risk),
+      .groups = "keep") %>%
+    dplyr::arrange(.data$mean_risk)
+
+  return(emp_risk)
+}
+
+################################################################################
+#' Showing Best Estimator Within Each Class of Estimators
+#'
+#' @description The \code{bestInClass} function finds the best performing
+#'  estimator within each class of estimator passed to \code{cvCovEst} and
+#'  finds the associated hyper-parameters if applicable.
+#'
+#' @param dat The table of empirical risks which is output by \code{cvCovEst}.
+#' @param worst This facilitates the option to choose the worst performing
+#'  estimator in each class.  Default is \code{FALSE}.
+#'
+#' @return A \code{data.frame} with rows corresponding to estimator classes and
+#'  columns for hyperparameter values and empirical risk for the best estimator
+#'  in that class.
+#'
+#' @importFrom dplyr group_by summarize arrange first %>%
+#' @importFrom rlang .data
+#'
+#' @keywords internal
+bestInClass <- function(dat, worst = FALSE) {
+
+  if (worst) {
+    worst_est <- dat %>%
+      dplyr::group_by(.data$estimator) %>%
+      dplyr::summarise(
+        hyperparameter = dplyr::last(.data$hyperparameters),
+        empirical_risk = dplyr::last(.data$empirical_risk),
+        .groups = "keep") %>%
+      dplyr::arrange(.data$empirical_risk)
+
+    return(worst_est)
+
+  }
+  else{
+    best_est <- dat %>%
+      dplyr::group_by(.data$estimator) %>%
+      dplyr::summarise(
+        hyperparameter = dplyr::first(.data$hyperparameters),
+        empirical_risk = dplyr::first(.data$empirical_risk),
+        .groups = "keep") %>%
+      dplyr::arrange(.data$empirical_risk)
+
+    return(best_est)
+
+  }
+}
+
+################################################################################
+#' Summarize Empirical Risks by Class with Hyperparameter
+#'
+#' @description The \code{hyperRisk} function groups together estimators of the
+#'  same class and parses the hyperparameter values over quantiles of the risk.
+#'
+#' @param dat The table of empirical risk calculations which is output by
+#'  \code{cvCovEst}.
+#'
+#' @return A named \code{list} of data frames. Each list element corresponds to
+#'  a \code{data.frame} of summary statistics for a specific estimator class.
+#'  If no estimators have hyper-parameters, a message is returned.
+#'
+#' @importFrom dplyr filter mutate first %>%
+#' @importFrom stats quantile
+#'
+#' @keywords internal
+hyperRisk <- function(dat) {
+  # These are the estimators with hyperparameters
+  has_hypers <- c(
+    "linearShrinkEst", "thresholdingEst",
+    "bandingEst", "taperingEst",
+    "scadEst", "poetEst", "robustPoetEst",
+    "adaptiveLassoEst"
+  )
+
+  estimators <- unique(dat$estimator)
+
+  if (any(has_hypers %in% estimators)) {
+
+    hyper_est <- estimators[which(estimators %in% has_hypers)]
+
+    hyper_summ <- lapply(hyper_est, function(est) {
+
+      h <- dat %>%
+        dplyr::filter(.data$estimator == est) %>%
+        dplyr::mutate(empirical_risk = round(.data$empirical_risk))
+
+      risk_stats <- quantile(
+        h$empirical_risk,
+        probs = c(0, 0.25, 0.50, 0.75, 1),
+        type = 3
+      )
+
+      hyper_risk <- sapply(unname(risk_stats), function(r) {
+        # Filter by the quantiles of the empirical risk
+        hr <- h %>%
+          dplyr::filter(.data$empirical_risk == r)
+
+        vec <- c(
+          dplyr::first(hr$hyperparameters),
+          dplyr::first(hr$empirical_risk)
+        )
+
+        return(vec)
+      })
+
+      df <- data.frame(
+        t(hyper_risk),
+        row.names = c(
+          "min",
+          "Q1", "median", "Q3",
+          "max"
+        )
+      )
+
+      colnames(df) <- c("hyperparameters", "empirical_risk")
+
+      return(df)
+    })
+
+    # Named list of data.frames corresponding to each estimator class
+    names(hyper_summ) <- hyper_est
+  }
+  else{
+    hyper_summ <- NULL
+    message("No candidate estimators have hyperparameters. hyperRisk = NULL")
+  }
+  return(hyper_summ)
+}
+
+################################################################################
+#' Generic Summary Method for cvCovEst
+#'
+#' @description The \code{summary} method provides summary statistics regarding
+#'  the performance of \code{cvCovEst} and can be used for diagnostic plotting.
+#'
+#' @param object A named \code{list} of class \code{"cvCovEst"}.
+#' @param summ_fun A character vector specifying which summaries to output.
+#' @param ... Additional arguments passed to summary method.
+#'
+#' @return A named \code{list} where each element corresponds to the output of
+#' of the requested summaries.
+#'
+#' @importFrom rlang exec
+#'
+#' @export
+summary.cvCovEst <- function(
+  object,
+  summ_fun = c(
+    "empRiskByClass",
+    "bestInClass",
+    "worstInClass",
+    "hyperRisk"),
+  ...
+) {
+
+  summary_functions <- c(
+    "empRiskByClass", "bestInClass", "worstInClass", "hyperRisk")
+
+  # Check cvCovEst credentials
+  checkPlotSumArgs(
+    object,
+    which_fun = "summary",
+    summ_fun = summ_fun)
+
+  risk_dat <- object$risk_df
+
+  sums_to_exec <- summary_functions[which(
+    summary_functions %in% summ_fun)]
+
+  out <- lapply(sums_to_exec, function(sum_fun) {
+    if (sum_fun == "worstInClass") {
+      f <- rlang::exec("bestInClass", risk_dat, worst = TRUE)
+    }
+    else{
+      f <- rlang::exec(sum_fun, risk_dat)
+    }
+    return(f)
+  })
+
+  names(out) <- sums_to_exec
+  return(out)
+}
