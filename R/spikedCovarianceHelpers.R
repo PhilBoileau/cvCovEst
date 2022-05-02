82120f305af5976cdd678ff1da8f44cb5e5acf9a
Philippe Boileau <pboileau.swim@gmail.com>
2022-05-02 12:08:57 -0700

    documenting spiked covariance estimator helpers

--- R/spikedCovarianceHelpers.R
+++ R/spikedCovarianceHelpers.R
@@ -37,7 +37,33 @@ estimateNoise <- function(eig_vals, p_n_ratio) {
   return(noise)
 }
 
+################################################################################
 
+#' Estimate Noise in Spiked Covariance Matrix Model
+#'
+#' @description \code{scaleEigVals()} computes the scaled eigenvalues, and
+#'   filters out all eigenvalues that do not need to be shrunk.
+#'
+#' @param eig_vals A \code{numeric} vector of estimated eigenvalues.
+#' @param noise  \code{numeric} representing the known scalar multiple of the
+#'   identity matrix giving the approximate population covariance matrix.
+#' @param p_n_ratio A \code{numeric} indicating the asymptotic ratio of the
+#'   number of features, p, and the number of observations, n. This ratio is
+#'   assumed to be between 0 and 1.
+#' @param num_spikes \code{numeric} integer equal to or larger than one which
+#'   providing the known number of spikes in the population covariance matrix.
+#'   If set to \code{NULL}, the number of spikes is estimated.
+#'
+#' @return A \code{numeric} vector of the scaled eigenvalues to be shrunk.
+#'
+#' @keywords internal
+#'
+#' @examples
+#' eig_vals <- c(15, 12, 9, 3, 3, 3)
+#' noise <- 3
+#' p_n_ratio <- 0.8
+#' scaleEigVals(eig_vals, noise, p_n_ratio, num_spikes = NULL)
+#'
 scaleEigVals <- function(eig_vals, noise, p_n_ratio, num_spikes) {
 
   # scale the eigenvalues
@@ -60,6 +86,34 @@ scaleEigVals <- function(eig_vals, noise, p_n_ratio, num_spikes) {
   return(scaled_eig_vals)
 }
 
+################################################################################
+
+#' Estimate Noise in Spiked Covariance Matrix Model
+#'
+#' @description \code{computeEll()} computes the ell value described in
+#'   \insertCite{donoho2018;textual}{cvCovEst}.
+#'
+#' @param scaled_eig_vals A \code{numeric} vector of scaled estimated
+#'   eigenvalues.
+#' @param p A \code{numeric} integer indicating the number of features in the
+#'   data.
+#' @param p_n_ratio A \code{numeric} indicating the asymptotic ratio of the
+#'   number of features, p, and the number of observations, n. This ratio is
+#'   assumed to be between 0 and 1.
+#'
+#' @return A \code{numeric} vector.
+#'
+#' @references
+#'   \insertAllCited{}
+#'
+#' @keywords internal
+#'
+#' @examples
+#' scaled_eig_vals <- c(10)
+#' p <- 10
+#' p_n_ratio <- 0.5
+#' computeEll(scaled_eig_vals, p, p_n_ratio)
+#'
 computeEll <- function(scaled_eig_vals, p, p_n_ratio) {
 
   # get the number of spikes
